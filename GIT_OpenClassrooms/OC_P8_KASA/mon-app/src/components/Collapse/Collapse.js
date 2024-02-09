b3d5a86ad4d7f2f086e8e1a64a12777c5516e7ee
Simon-LM <simon.ln.dev@pm.me>
2024-02-09 13:54:12 +0100

    Check 09/02/2024 1354

---
+++ GIT_OpenClassrooms/OC_P8_KASA/mon-app/src/components/Collapse/Collapse.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import arrowIcon from '../../assets/images/arrow_about.svg'
+import './Collapse.scss'
+
+function Collapse({ title, content, className }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleToggle = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div className={`collapse ${className}`}>
+      <div className={`collapse__title ${className}-title`}>
+        <span>{title}</span>
+        <button
+          onClick={handleToggle}
+          className={`collapse__arrow ${isOpen ? 'rotate' : ''}`}
+        >
+          <img
+            src={arrowIcon}
+            alt="flèche pour ouvrir"
+            // onClick={handleToggle}
+            className={`collapse__img`}
+          />
+        </button>
+      </div>
+      <div
+        className={`collapse__content ${className}-content ${
+          isOpen ? 'open' : ''
+        }`}
+      >
+        {content}
+      </div>
+    </div>
+  )
+}
+export default Collapse
---
+++ GIT_OpenClassrooms/OC_P8_KASA/mon-app/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import whiteLogo from '../../assets/images/Logo_Kasa_white.png'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <img src={whiteLogo} className="footer__logo" alt="logo" />
+      <p className="footer__legal-info">©2020 Kasa. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
